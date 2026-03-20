801304b95a7b119aaf2e17b7b02e760eb619f0b3
cafeina3325-lab <cafeina3325@gmail.com>
2026-03-20 10:08:19 +0900

    again

--- src/lib/get-session.ts
+++ src/lib/get-session.ts
@@ -3,6 +3,6 @@ import { cookies } from 'next/headers';
 import { sessionOptions, SessionData } from './session';
 
 export async function getSession() {
-  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+  const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
   return session;
 }
